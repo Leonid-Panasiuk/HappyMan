1cf4b5ee0094166bba5484f04d25e3fb4dca2b00
Leonid-Panasiuk <lona.panasuk@gmail.com>
2020-08-27 21:30:58 +0300

    add

--- send.php
+++ send.php
@@ -1,91 +1,26 @@
 <?php
-$name = stripslashes(htmlspecialchars($_POST['name']));
-$phone = stripslashes(htmlspecialchars($_POST['phone']));
-$cart = stripslashes(htmlspecialchars($_POST['cart']));
-$payment = stripslashes(htmlspecialchars($_POST['payment']));
-$department = stripslashes(htmlspecialchars($_POST['department']));
-$city = stripslashes(htmlspecialchars($_POST['city']));
-$course = stripslashes(htmlspecialchars($_POST['course']));
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $cart = $_POST['cart'];
+        $payment = $_POST['payment'];
+        $department = $_POST['department'];
+        $city = $_POST['city'];
+        $course = $_POST['course'];
+        $from = 'From: HappyMan'; 
+        $to = '380974682460@ya.ru'; // send to this address
+        $subject = 'HappyMan'; //subject line in email
+        $success_url = './form-ok.php';
+
+         $message = "Від: $name\n
+         Телефон: $phone\n 
+         Доставка: $cart\n 
+         Місто:$city\n
+         Відділення: $department\n
+         Оплата: $payment\n 
+         Курс: $course\n";
 
 
-
-
-// if(empty($city) || empty($department)){
-// echo '<h1 style="color:red;">Пожалуйста заполните все поля</h1>';
-// echo '<meta http-equiv="refresh" content="2; url=http://'.$_SERVER['SERVER_NAME'].'">';
-// }
-
-if(empty($city) || empty($department)) {
-    $subject = '[HappyMan]'; 
-    $addressat = "lona.panasuk@gmail.com";
-    $success_url = './form-ok.php?name='.$_POST['name'].
-    '&phone='.$_POST['phone'].
-    '&cart='.$_POST['cart'].
-    '&payment='.$_POST['payment'].
-    '&course='.$_POST['course'];
-    
-    
-    /////
-    $message = "Имя: {$name}
-    \nТелефон: {$phone}
-    \nДоставка: {$cart}
-    \nКурс: {$course}
-    \nОплата: {$payment}";
-    
-    $verify = mail($addressat,$subject,$message, "From:Mail", "Content-type:text/plain;charset=utf-8\r\n");
-    
-    if ($verify == 'true'){
-        header('Location: '.$success_url);
-        echo '<h1 style="color:green;">Поздравляем! Ваш заказ принят!</h1>';
+         echo '<h1 style="color:green;">Вітаємо! Ваше замовлення прийнято!</h1>';
         
-        exit;
-    }
-    else 
-        {
-            $errorMessage = error_get_last()['message'];
-            $result = '<h1 style="color:red;">Произошла ошибка!</h1><p>' . $errorMessage . '</p>';
-            echo $result;
-        }
-    }
+        mail($to, $subject, $message); ?>
 
-
-
-///////IF CYTY AND DEPARTNEMT = TRUE!!!!
-
-else{
-$subject = '[HappyMan]'; 
-$addressat = "lona.panasuk@gmail.com";
-$success_url = './form-ok.php?name='.$_POST['name'].
-'&phone='.$_POST['phone'].
-'&cart='.$_POST['cart'].
-'&payment='.$_POST['payment'].
-'&department='.$_POST['department'].
-'&city='.$_POST['city'].
-'&course='.$_POST['course'];
-
-
-/////
-$message = "Имя: {$name}
-\nТелефон: {$phone}
-\nДоставка: {$cart}
-\nКурс: {$course}
-\nГород: {$city}
-\nОтделение: {$department}
-\nОплата: {$payment}";
-
-$verify = mail($addressat,$subject,$message, "From:Mail", "Content-type:text/plain;charset=utf-8\r\n");
-
-if ($verify == 'true'){
-    header('Location: '.$success_url);
-    echo '<h1 style="color:green;">Поздравляем! Ваш заказ принят!</h1>';
-    
-    exit;
-}
-else 
-    {
-        $errorMessage = error_get_last()['message'];
-        $result = '<h1 style="color:red;">Произошла ошибка!</h1><p>' . $errorMessage . '</p>';
-        echo $result;
-    }
-}
-?>
