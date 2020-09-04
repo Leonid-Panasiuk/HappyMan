b2de60c7409deef45b83f166b8f73a870e085263
Leonid Panasiuk <51416748+Leonid-Panasiuk@users.noreply.github.com>
2020-09-04 10:17:51 +0300

    add

--- js/app.js
+++ js/app.js
@@ -1,52 +1,86 @@
 new WOW().init();
 
-
-
 // Animate loader
-$(window).on('load', function() {
+$(window).on('load', function () {
 
   $(".preload").fadeOut(2000);
 });
 
+// link platform detect
+
+var isMobile = {
+  Android: function() {
+      return navigator.userAgent.match(/Android/i);
+  },
+  iOS: function() {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+};
+
+if (isMobile.Android()) {
+  ios = document.getElementById("ios")
+  ios.style.display = "none";
+}
+else if(isMobile.iOS())
+{
+  android = document.getElementById("andoid")
+  android.style.display = "none";
+};
+
+// for Windows
+
+$(document).ready(function() {
+  p = navigator.platform;
+
+  if( p === 'Win32') {
+    android = document.getElementById("andoid")
+    android.style.display = "none";
+    ios = document.getElementById("ios")
+    ios.style.display = "none";
+  }
+})
+
+
+
 //1
 $(document).ready(function () {
   $(".show-btn-1").click(function () {
-      $(".text-show-1").toggle(700);
+    $(".text-show-1").toggle(700);
   });
 });
 
 //2
 $(document).ready(function () {
   $(".show-btn-2").click(function () {
-      $(".text-show-2").toggle(700);
+    $(".text-show-2").toggle(700);
   });
 });
 
 //3
 $(document).ready(function () {
   $(".show-btn-3").click(function () {
-      $(".text-show-3").toggle(700);
+    $(".text-show-3").toggle(700);
   });
 });
 
 //4
 $(document).ready(function () {
   $(".show-btn-4").click(function () {
-      $(".text-show-4").toggle(700);
+    $(".text-show-4").toggle(700);
   });
 });
 
 //5
 $(document).ready(function () {
   $(".show-btn-5").click(function () {
-      $(".text-show-5").toggle(700);
+    $(".text-show-5").toggle(700);
   });
 });
 
 //selected delivery
 
-$(function() {
-  $("#graph_select").change(function() {
+$(function () {
+  $("#graph_select").change(function () {
     if ($("#new-post").is(":selected")) {
       $(".show-form").show(700);
       $(".show-form").hide(700);
@@ -65,5 +99,4 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     scrollTop: $($.attr(this, 'href')).offset().top
   }, 1000);
 });
-
 
